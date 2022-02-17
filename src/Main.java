2ef608b0aa70090bd58731c3e139adae612f4415
Thomas Belcher <tbelcher@verizon.net>
2022-02-17 14:04:38 -0500

    updating main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
 public class Main {
 
     public static void main(String[] args) {
+        int studentAge = 15;
+        double studentGPA = 3.45;
+        char studentFirstInitial = 'K';
+        char studentLastInitial = 'V';
+        boolean hasPerfectAttendance = true;
         
+      System.out.println("First Initial: " + studentFirstInitial);
+      System.out.println("Last Initial: " + studentLastInitial);
+      System.out.println("Age: " + studentAge);
+      System.out.println("GPA: " + studentGPA);
+      System.out.println("Perfect Attenadance? " + hasPerfectAttendance);
+
+      
     }
 }
