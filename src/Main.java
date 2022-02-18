de2baa5b4f074bf897fb97b1e9f0feb4644df954
Thomas Belcher <tbelcher@verizon.net>
2022-02-18 16:44:15 -0500

    adding to project

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,21 @@ public class Main {
     public static void main(String[] args) {
         int studentAge = 15;
         double studentGPA = 3.45;
-        char studentFirstInitial = 'K';
-        char studentLastInitial = 'V';
+        String firstStudentName = "Herbert";
+        String lastStudentName = "Loser";
+        char studentFirstInitial = firstStudentName.charAt(0);
+        char studentLastInitial = 'L';
         boolean hasPerfectAttendance = true;
+
+
         
       System.out.println("First Initial: " + studentFirstInitial);
       System.out.println("Last Initial: " + studentLastInitial);
       System.out.println("Age: " + studentAge);
       System.out.println("GPA: " + studentGPA);
       System.out.println("Perfect Attenadance? " + hasPerfectAttendance);
-
+      System.out.println("Student First Name: " + firstStudentName);
+      System.out.println("Student Last Name: " + lastStudentName);
       
     }
 }
