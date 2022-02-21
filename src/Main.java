e672f65e6a392c61d6fe8577b96ee92a6e2b3514
Thomas Belcher <tbelcher@verizon.net>
2022-02-20 21:01:25 -0500

    adding code for challenge

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,39 @@
 import java.util.Scanner;
-
 public class Main {
-
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        boolean isOnRepeat = true;
-        while(isOnRepeat) {
-            System.out.println("Playing current song");
-            System.out.println("Would you like to take this song off of repeat? If so, answer yes");
-            String userInput = input.next();
-
-            if(userInput.equals("yes")) {
-                isOnRepeat = false;
-            }
-        }
-        System.out.println("Playing next song");
+  public static void main(String[] args) {
+    String question = "Who is the greatest rock guitarist?";
+    String choiceOne = "Eric Clapton";
+    String choiceTwo = "Jimmy Page";
+	String choiceThree = "Jimi Hendrix";
+    String correctAnswer = "Jimmy Page";
+    String artistChoice1 = " ";
+    String artistChoice2 = " ";
+  	String artistChoice = " ";
+  	Boolean loopComplete = false;
+    do {
+  	System.out.println(question);
+    System.out.println("You may answer:  "+ choiceOne + " or " + choiceTwo + " or " + choiceThree);
+	    Scanner input = new Scanner(System.in);
+	    artistChoice1 = input.next();
+	    artistChoice2 = input.next();
+	    artistChoice = artistChoice1 + " " + artistChoice2;
+	    System.out.println("You answered " + artistChoice);
+		if (artistChoice.equals(choiceOne)||artistChoice.equals(choiceTwo)||artistChoice.equals(choiceThree))
+		{
+			if(artistChoice.equals(correctAnswer)) {
+	           System.out.println("You are a wise person picking " + choiceTwo + "!!!");
+	           loopComplete = true;
+	        }
+	        else {
+	           System.out.println("Keep listening. "+artistChoice + " is good, but at some point you'll pick " + choiceTwo + "!!!");
+	           loopComplete = true;
+	        }
+		}
+		else {
+			System.out.println("Please select one of the offered artists - try again");  	
+		}
     }
-}
+	while (!loopComplete);
+    } 
+   
+	}
